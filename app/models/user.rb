5d745ee7ec3ac4a580523494105d66e3cee8faa8
priyal558 <priyalg@shriffle.com>
2021-02-02 18:39:46 +0530

    after polymorphic association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_one :addresses, as: :addressable
 	mount_uploader :image, ProfileUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
